3a16a14e95ca827833864efb708a7feacdf50e13
susanliubc <susanliubc@gmail.com>
2020-08-17 17:22:49 -0700

    debugging server side

--- tutormanager/accounts/serializers.py
+++ tutormanager/accounts/serializers.py
@@ -17,7 +17,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': Ture}}
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
--- tutormanager/accounts/urls.py
+++ tutormanager/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from knox import views as knox_views
-from .api import LoginAPI
+from .api import RegisterAPI, LoginAPI, UserAPI
 
-urlpatthers = [
+urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/register', RegisterAPI.as_view()),
     path('api/auth/login', LoginAPI.as_view()),
